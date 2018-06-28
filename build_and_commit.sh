3b44afdfdfcdcf13226573088e94cc51cd47988f
sapristi <megabigfoutoir@gmail.com>
2018-06-28 21:53:38 +0000

    website source

--- build_and_commit.sh
+++ build_and_commit.sh
@@ -1,24 +1,29 @@
-#/bin/bash
+#!/bin/bash
 
 
-
-
-cd gen/input/achem
+cd input/achem
 
 for f in $(ls | grep  .org$)
 do
-    emacs $f --load ~/.emacs --batch -f org-md-export-to-markdown
+    emacs $f --load ~/.emacs.d/init.el --batch -f org-md-export-to-markdown
     echo "exported " + $f + " as markdown" 
 done
 cd ../..
 
-poole.py --build
+python2 poole.py --build
 echo "built website"
 
+git add -u
+git commit -m "website source"
+if [ -n $1 -a $1 = "push" ]
+then 
+    git push
+fi
+
 cd ..
-cp -r gen/output/* .
+cp -r ./blog_src/output/* ./sapristi.github.io/
 
-
+cd sapristi.github.io
 
 git add -u
 git add *.html
